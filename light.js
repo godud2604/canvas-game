a7cdd6f5e12407a559dd3c7fbb94495c13618bbb
godud2604 <ees238@naver.com>
2021-09-27 05:25:01 +0900

    💄 Add gradients color

--- light.js
+++ light.js
@@ -14,10 +14,19 @@ class Particle {
 }
 
 class Line {
-  constructor(x, y) {
+  constructor(index, x, y) {
     this.x = x;
     this.y = y;
     this.height = 300;
+
+    const gradientStartY = cm.canvasHeight - (this.height + (cm.canvasHeight - this.y));
+    this.gradient = cm.context.createLinearGradient(
+      0, gradientStartY,
+      0, this.y
+    );
+    this.gradient.addColorStop(0, `rgba(${cm.colors2[index]}, 0)`);
+    this.gradient.addColorStop(0.5, `rgba(${cm.colors2[index]}, 0.5)`);
+    this.gradient.addColorStop(1, `rgba(${cm.colors2[index]}, 0.8)`);
 
     const numberOfParticles = 30;
     this.particles = [];
@@ -29,6 +38,9 @@ class Line {
   }
 
   draw() {
+    cm.context.fillStyle = this.gradient;
+    cm.context.strokeStyle = this.gradient;
+
     cm.context.beginPath();
     cm.context.moveTo(this.x, this.y);
     cm.context.lineTo(this.x, this.y - this.height);
@@ -50,25 +62,71 @@ class Line {
 }
 
 class Light {
-  constructor(x, y) {
+  constructor(index, x, y) {
     this.x = x;
     this.y = y;
     this.width = 20;
     this.height = 300;
+    this.angle = 0;
 
     const numberOfLines = 5;
     this.lines = [];
     for (let i = 0; i < numberOfLines; i++) {
       this.lines.push(
         new Line(
+          index,
           this.x + (Math.random() * this.width - this.width * 0.5),
           this.y
         )
       );
     }
+
+    this.gradient = cm.context.createLinearGradient(
+      0, cm.canvasHeight - (this.height + (cm.canvasHeight - this.y)),
+      0, this.y
+    );
+    this.gradient.addColorStop(0, `rgba(${cm.colors[index]}, 0)`);
+    this.gradient.addColorStop(0.5, `rgba(${cm.colors[index]}, 0.5)`);
+    this.gradient.addColorStop(0.75, `rgba(${cm.colors[index]}, 0.5)`);
+    this.gradient.addColorStop(1, `rgba(${cm.colors[index]}, 1)`);
   }
 
   draw() {
+    cm.context.fillStyle = this.gradient;
+
+    cm.context.save(); // canvas 드로잉 상태 저장 (save, restore)
+    cm.context.filter = 'blur(20px)';
+    cm.context.beginPath();
+    cm.context.ellipse( // 타원
+      this.x,
+      this.y,
+      this.width * 2 + Math.abs(Math.sin(this.angle * Math.PI/180 * 30)) * 5,
+      this.width * 0.5 + Math.abs(Math.sin(this.angle * Math.PI/180 * 30)) * 5,
+      0, 0, Math.PI * 2,
+    );
+    cm.context.fill();
+
+    cm.context.filter = 'blur(5px)';
+    cm.context.beginPath();
+    cm.context.ellipse( // 타원
+      this.x,
+      this.y,
+      this.width + Math.abs(Math.sin(this.angle * Math.PI/180 * 30)) * 5,
+      this.width * 0.25 + Math.abs(Math.sin(this.angle * Math.PI/180 * 30)) * 5,
+      0, 0, Math.PI * 2,
+    );
+    cm.context.fill();
+    this.angle++;
+
+    cm.context.fillRect(
+      this.x - this.width * 0.5,
+      cm.canvasHeight - (this.height + (cm.canvasHeight - this.y)),
+      this.width,
+      this.height
+    );
+
+    cm.context.restore();
+
     let line;
     for (let i = 0; i < this.lines.length; i++) {
       line = this.lines[i];
--- main.js
+++ main.js
@@ -2,15 +2,33 @@ const cm = {
   canvas: undefined,
   context: undefined,
   canvasWidth: 0,
-  canvasHeight: 0
+  canvasHeight: 0,
+  colors: [
+    '222, 35, 18', // red
+    '238, 150, 63', // orange
+    '246, 228, 0', // yellow
+    '110, 210, 70', // green
+    '63, 145, 255', // blue
+    '185, 22, 226', // purple
+  ],
+  colors2: [
+    '255, 160, 150', // red
+    '255, 200, 150', // orange
+    '255, 250, 180', // yellow
+    '195, 255, 170', // green
+    '220, 220, 255', // blue
+    '239, 173, 255', // purple
+  ],
 };
 
 (function () {
   cm.canvas = document.querySelector('#the-canvas');
   cm.context = cm.canvas.getContext('2d');
   const canvasContainer = document.querySelector('.canvas-container');
-  // const dpr = window.devicePixelRatio > 1 ? 2 : 1; // 고해상도 처리를 하기 위함. // 단점 : width, height가 커짐에 따라 상응되는 퍼포먼스 처리가 많아짐.
   const dpr = 1;
+  const mouse = { x: 0, y: 0 };
+  const lights = [];
+  let indexOfLight = 0;
 
   function setSize() {
     cm.canvasWidth = canvasContainer.clientWidth;
@@ -26,13 +44,29 @@ const cm = {
     draw();
   }
 
-  const light = new Light(100, 400);
 
   function draw() {
     cm.context.clearRect(0, 0, cm.canvasWidth, cm.canvasHeight);
-    light.draw();
-    requestAnimationFrame(draw); // draw 반복실행 하면서 애니메이션 실행
+
+    for (let i = 0; i < lights.length; i++) {
+      lights[i].draw();
+    }
+
+    requestAnimationFrame(draw); 
   }
+
+  cm.canvas.addEventListener('click', function(e) {
+    if (indexOfLight >= cm.colors.length) return;
+    // mouse.x = e.layerX;
+    // mouse.y = e.layerY;
+    mouse.x = e.clientX - cm.canvas.getBoundingClientRect().left;
+    mouse.y = e.clientY - cm.canvas.getBoundingClientRect().top;
+
+    const light = new Light(indexOfLight, mouse.x, mouse.y);
+    lights.push(light);
+
+    indexOfLight++;
+  });
 
   window.addEventListener('resize', setSize);
   window.addEventListener('load', setup);
